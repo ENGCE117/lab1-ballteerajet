bd4087d51b5709194d789ff09a336186169133c8
ballteerajet <teerajet009@gmail.com>
2025-03-03 00:39:42 +0700

    First try

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void GetMatrix(int **value, int *row, int *col) {
+    scanf("%d %d", row, col);
+    *value = (int *)malloc((*row) * (*col) * sizeof(int));
+    
+    for (int i = 0; i < (*row); i++) {
+        for (int j = 0; j < (*col); j++) {
+            scanf("%d", (*value) + (i * (*col) + j));
+        }
+    }
+}
+
+void PrintMatrix(int *value, int row, int col) {
+    printf("Matrix (%dx%d):\n", row, col);
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            printf("%d ", *(value + (i * col) + j));
+        }
+        printf("\n");
+    }
+}
 
 int main() {
-    //YOUR CODE HERE
-    return 0 ;
-}//end function
+    int *data, m, n;
+    GetMatrix(&data, &m, &n);
+    PrintMatrix(data, m, n);
+    free(data);
+    return 0;
+}
